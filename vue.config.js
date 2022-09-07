8df6170efb3e21f7fe3216b7b63464f99a2347d9
Stepa <some-to-do@yandex.ru>
2022-09-07 20:10:04 +0300

    formatted vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: 'cadastral-engineer'
+    publicPath: 'cadastral-engineer',
   })
